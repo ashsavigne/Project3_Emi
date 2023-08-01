f163aa098a8795d5e9dcce43c4cbb349c4c93b4b
Ash Savigne <108949209+ashsavigne@users.noreply.github.com>
2023-08-01 13:34:37 -0400

    Update song.cpp

--- song.cpp
+++ song.cpp
@@ -2,138 +2,120 @@
 #include "song.h"
 #include <iostream>
 #include <string>
-#include <sstream>
-using namespace std;
 
-void Playlist::Insert(Song* song, const string& option, const string& adj, bool exp, int year, const string& artist){
-    if ((exp != song->exp) || (artist != "any" && artist != song->artist)){
+void Playlist::Insert(Song* song, const string& adj, bool exp, int year, const string& artist){
+    if ((!exp && song->explicit_cont) || (year != 0 && year >= song->year && song->year <= year + 10) || (artist != "any" && artist != song->artist)){
         delete song;
         return;
     }
+    else if ((artist == song->artist && adj == "any" ) || (year == song->year && adj == "any")){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100;
+        songs.push_back(song);
 
-    // Check if the songs year is within the decade inserted by user
-    else if (option == "decade") {
-        int lower = ((song->year % 100) / 10) * 10;
-        int upper = lower + 10;
-        if (year%100 >= lower && year%100 <= upper) {
-            song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-            songs.push_back(song);
-        }
     }
-    else if (year == 0 || year == song->year) {
-        if (option == "mood") {
-            if (adj == "happy" && song->tempo >= 120 && song->tempo <= 140 && song->dance >= 60 && song->dance <= 80 &&
-                song->energy >= 60 && song->energy <= 80 && song->valence >= 60 && song->valence <= 80) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-                songs.push_back(song);
-            } else if (adj == "sad" && song->tempo >= 60 && song->tempo <= 80 && song->energy <= 30 &&
-                       song->dance < 30 && song->loud < 40 && song->valence < 40) {
-                song->rating = 180 - song->tempo + (100 - song->loud) + (100 - song->dance) + (100 - song->energy);
-                songs.push_back(song);
-            } else if (adj == "relaxed" && song->tempo >= 60 && song->tempo <= 80 && song->energy >= 40 &&
-                       song->energy <= 60 && song->dance >= 40 && song->dance <= 60 && song->loud <= 20 &&
-                       song->valence >= 40 && song->valence <= 60 && song->loud <= 40) {
-                song->rating = 180 - song->tempo + (100 - song->loud) + (100 - song->dance) + (100 - song->energy);
-                songs.push_back(song);
-            } else if (adj == "victory" && song->tempo >= 100 && song->loud >= 80) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-                songs.push_back(song);
-            } else if (adj == "angry" && song->tempo >= 140 && song->valence <= 50 && song->energy >= 80 &&
-                       song->dance >= 80) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100 + song->loud;
-                songs.push_back(song);
-            }
-        } else if (option == "event") {
-            if (adj == "dinner" && song->tempo >= 60 && song->tempo <= 80 && song->dance <= 60 && song->energy <= 20 &&
-                song->loud <= 20) {
-                song->rating = 180 - song->tempo + (100 - song->loud) + (100 - song->dance) + (100 - song->energy);
-                songs.push_back(song);
-            } else if (adj == "party" && song->tempo >= 120 && song->tempo <= 140 && song->energy >= 80 &&
-                       song->dance >= 80 && song->valence >= 80) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-                songs.push_back(song);
-            } else if (adj == "workout" && song->tempo >= 140 && song->valence >= 50 && song->energy >= 80 &&
-                       song->dance >= 80 && song->loud >= 60) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100 + song->loud * 100;
-                songs.push_back(song);
-            } else if (adj == "after party" && song->tempo >= 80 && song->tempo <= 120 && song->dance <= 50 &&
-                       song->dance >= 30 && song->energy <= 50 && song->energy >= 30 && song->valence >= 60 &&
-                       (song->key == 10 || song->key == 8) && song->loud <= 50) {
-                song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-                songs.push_back(song);
-            }
-        } else if (option == "artist") {
-            song->rating = song->tempo + song->dance * 100 + song->energy * 100;
-            songs.push_back(song);
-        }
+    else if (adj == "happy" && song->tempo >= 120 && song->tempo <= 140 && song->dance >= 70 && song->energy >= 50 && song->energy <= 90 && song->valence >= 70 && song->valence <= 90 && song->mode == 1 && song->loud >= -20) {
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100;
+        songs.push_back(song);
     }
-
+    else if (adj == "sad" && song->tempo >= 50 && song->tempo <= 80 && song->energy <= 40 && song->dance <= 40 && song->loud <= -3 && song->valence <= 40 && song->mode == 0 && song->loud <= -30){
+        song->rating = 180 - song->tempo + (100 - song->dance) + (100 - song->energy);
+        songs.push_back(song);
+    }
+    else if (adj == "relax" && song->tempo >= 60 && song->tempo <= 90 && song->energy >= 10 && song->energy <= 50 && song->dance <= 40 && song->loud >= -40 && song->loud <= -20 && song->valence >= 30 && song->valence <= 70 && song->mode == 0){
+        song->rating = 180 - song->tempo + (100 - song->dance) + (100 - song->energy);
+        songs.push_back(song);
+    }
+    else if (adj == "victory" && song->tempo >= 120 && song->tempo <= 140 && song->valence >=70 && song->energy >= 50 && song->energy <= 90 && song->dance >= 70 && song->mode == 1){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100;
+        songs.push_back(song);
+    }
+    else if (adj == "angry" && song->tempo >= 130 && song->valence <= 60 && song->valence >= 30 && song->energy >= 50 && song->energy >= 70 && song->dance >= 70 && song->loud >= -10){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100 + song->loud;
+        songs.push_back(song);
+    }
+    else if (adj == "Din" && song->tempo >= 50 && song->tempo <= 80 && song->dance <= 50 && song->dance >= 10 && song->energy >= 70 && song->loud >= -40 && song->loud <= -20){
+        song->rating = 180 - song->tempo + (100 - song->dance) + (100 - song->energy);
+        songs.push_back(song);
+    }
+    else if (adj == "party" && song->tempo >= 120&& song->energy >= 80 && song->dance >= 80 && song->valence >= 80 && song->loud >= -20 && song->mode == 1){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100;
+        songs.push_back(song);
+    }
+    else if (adj == "workout" && song->tempo >= 100 && song->tempo <= 150 && song->valence >= 50 && song->valence <= 90 && song->energy >= 70 && song->dance >= 50 && song->dance <= 90 && song->loud >= -10 && song->mode == 1){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100 + song->loud * 100;
+        songs.push_back(song);
+    }
+    else if (adj == "Afterparty" && song->tempo >= 90 && song->tempo <= 120 && song->dance >= 50 && song->dance <= 80 && song->energy <= 80 && song->energy >= 40 && song->valence >= 30 && song->valence <= 80 && song->loud >= -20){
+        song->rating = song->tempo + song->dance * 100 + song->energy * 100;
+        songs.push_back(song);
+    }
+    else if (adj == "Focus" && song->tempo <= 100 && song->tempo >= 60 && song->dance <= 40 && song->dance >= 10 && song->energy >= 10 && song->energy <= 50 && song->loud >= -40 && song->loud <= -20){
+        song->rating = (180 - song->tempo) + (100 - song->dance) + (100 - song->energy);
+        songs.push_back(song);
+    }
+    else if (adj == "Sleepy" && song->tempo >= 40 && song->tempo <= 60 && song->dance <= 30 && song->valence <= 40 && song->energy <= 30 && song->loud <= -30 && song->loud >= -60){
+        song->rating = (180 - song->tempo) + (100 - song->dance) + (100 - song->energy);
+        songs.push_back(song);
+    }
     else
         delete song;
 
-}
 
+}
 void Playlist::PrintAll(){
     if (songs.empty()){
-        cout << "Sorry! No applicable songs :(" << endl << endl;
+        cout << "Sorry! No applicable songs :(" << endl;
         return;
     }
-    cout << "Your playlist has " << songs.size() << " songs!" << endl << endl;
+    cout << "Your playlist has " << songs.size() << " songs!" << endl;
     for (const auto& i : songs){
         cout << "Title: " << i->name << endl;
         cout << "Artist: " << i->artist << endl;
-        cout << "Album: " << i->album << endl << endl;
+        cout << "Album: " << i->album << endl;
+        cout << "Rating: " << i->rating << endl;
+        cout << "exp: " << i->explicit_cont << endl << endl;
     }
 
 }
 
 void Playlist::PrintN(int n){
     if (songs.empty()){
-        cout << "Sorry! No applicable songs :(" << endl << endl;
+        cout << "Sorry! No applicable songs :(" << endl;
         return;
     }
     if (n > songs.size()){
         cout << "Your playlist has " << songs.size() << " songs!" << endl;
 
-        for (int i = songs.size() - 1; i > 0; i--) {
-            // FIXME for Event and mood - giving an exit code 139 (interrupted by signal 11: SIGSEGV)
-            cout << "Title: " << songs[i]->name << endl;
-            cout << "Artist: " << songs[i]->artist << endl;
-            cout << "Album: " << songs[i]->album << endl << endl;
-            cout << "Rating: " << songs[i]->rating << endl << endl;
+        for (const auto& i : songs){
+            cout << "Title: " << i->name << endl;
+            cout << "Artist: " << i->artist << endl;
+            cout << "Album: " << i->album << endl;
+            cout << "Rating: " << i->rating << endl << endl;
         }
     }
     else{
         cout << "Your playlist has " << n << " songs!" << endl;
-
-        for (int i = n - 1; i > 0; i--){
-            // FIXME for Event and mood - giving an exit code 139 (interrupted by signal 11: SIGSEGV)
+        for (int i = 0; i < n; i++){
             cout << "Title: " << songs[i]->name << endl;
             cout << "Artist: " << songs[i]->artist << endl;
-            cout << "Album: " << songs[i]->album << endl << endl;
+            cout << "Album: " << songs[i]->album << endl;
             cout << "Rating: " << songs[i]->rating << endl << endl;
 
         }
     }
-
 }
 
-void Song::Deserialize(istringstream& stream) {
+void Playlist::Song::Deserialize(istringstream& stream) {
     string token;
-    if (!getline(stream, token, ',')) // ignore id
-        throw runtime_error("Error: missing id");
+    getline(stream, token, ','); // ignore id
+    
+    getline(stream, this->name, ',');
 
-    if (!getline(stream, this->name, ','))
-        throw runtime_error("Error: missing name");
+    getline(stream, this->album, ',');
 
-    if (!getline(stream, this->album, ','))
-        throw runtime_error("Error: missing album");
+    getline(stream, token, ','); // ignore album id
 
-    if (!getline(stream, token, ',')) // ignore album id
-        throw runtime_error("Error: missing album id");
-
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing artist");
+    getline(stream, token, ',');
     try{
         token.erase(0, 2);
         token.erase(token.size() - 2, 2);
@@ -142,27 +124,22 @@ void Song::Deserialize(istringstream& stream) {
     catch (out_of_range&){
         this->artist = "null";
     }
+    getline(stream, token, ','); // ignore artist id
 
-    if (!getline(stream, token, ',')) // ignore artist id
-        throw runtime_error("Error: missing artist id");
+    getline(stream, token, ','); // ignore track num
 
-    if (!getline(stream, token, ',')) // ignore track num
-        throw runtime_error("Error: missing track num");
+    getline(stream, token, ','); // ignore disk num
 
-    if (!getline(stream, token, ',')) // ignore disk num
-        throw runtime_error("Error: missing disk num");
+    getline(stream, token, ',');
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing explicit"); // explicit
-
-    if (token == "TRUE")
-        this->exp = true;
-    else
-        this->exp = false;
-
-    if (!getline(stream, token, ',')){
-        throw runtime_error("Error: missing decidability");
+    if (token.size() == 4){
+        this->explicit_cont = true;
     }
+    else
+        this->explicit_cont = false;
+
+    getline(stream, token, ',');
+
     try{
         this->dance = int(stod(token) * 100);
     }
@@ -170,8 +147,8 @@ void Song::Deserialize(istringstream& stream) {
         this->dance = 0;
     }
 
-    if (!getline(stream, token, ',')) // ignore artist id
-        throw runtime_error("Error: missing energy");
+    getline(stream, token, ','); // ignore artist id
+
     try{
         this->energy = int(stod(token) * 100);
     }
@@ -179,8 +156,8 @@ void Song::Deserialize(istringstream& stream) {
         this->energy = 0;
     }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing artist id");
+    getline(stream, token, ',');
+
     try{
         this->key = stoi(token);
     }
@@ -188,33 +165,34 @@ void Song::Deserialize(istringstream& stream) {
         this->key = 0;
     }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("no loud");
+    getline(stream, token, ',');
+
     try{
-        this->loud = int(stod(token) * 100);
+        this->loud = stoi(token);
     }
     catch (invalid_argument&){
         this->loud = 0;
     }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing mode"); // ignore mode
+    getline(stream, token, ',');
+    try{
+        this->mode = stoi(token);
+    }
+    catch (invalid_argument&){
+        this->mode = -1;
+    }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing speech"); // ignore speece
+    getline(stream, token, ',');
+        // ignore speece
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing acoustics");
+    getline(stream, token, ',');
 
+    getline(stream, token, ',');
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing instrumental");
+    getline(stream, token, ',');
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing liveliness");
+    getline(stream, token, ',');
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing valence");
     try{
         this->valence = int(stod(token) * 100);
     }
@@ -222,8 +200,7 @@ void Song::Deserialize(istringstream& stream) {
         this->valence = 0;
     }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing tempo");
+    getline(stream, token, ',');
     try{
         this->tempo = stoi(token);
     }
@@ -232,14 +209,12 @@ void Song::Deserialize(istringstream& stream) {
     }
 
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing duration"); // ignore duration
+   getline(stream, token, ',');
+       // ignore duration
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing time signature");
+    getline(stream, token, ',');
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing year");
+    getline(stream, token, ',');
     try{
         this->year = stoi(token);
     }
@@ -247,8 +222,7 @@ void Song::Deserialize(istringstream& stream) {
         this->year = 0;
     }
 
-    if (!getline(stream, token, ','))
-        throw runtime_error("Error: missing release date");
+    getline(stream, token, ',');
 }
 
 Playlist::~Playlist(){
